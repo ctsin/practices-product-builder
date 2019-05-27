54a01b6eae99b17e1feb0d7d519793ae519fdf44
邢贵志 <tsing@outlook.com>
2019-05-27 18:00:32 +0800

    完成 model 页面布局

--- src/typescript/component/main.ts
+++ src/typescript/component/main.ts
@@ -1,3 +1,4 @@
 import { header } from "./header";
+import { model } from "./model";
 
-document.getElementById("root").innerHTML = [header()].join();
+document.getElementById("root").innerHTML = [header(), model()].join("");
---
+++ src/typescript/component/model.ts
@@ -0,0 +1,15 @@
+export const model = () => {
+  return /* html */ `
+    <div class="model">
+      <div class="model@set">
+        <input class="model@input" id="75d" name="model" type="radio" />
+        <label class="model@label" for="75d">75D</label>
+      </div>
+
+      <div class="model@set">
+        <input class="model@input" id="90d" name="model" type="radio" />
+        <label class="model@label" for="90d">90D</label>
+      </div>
+    </div>
+  `;
+};
