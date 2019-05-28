b142670823aa8e1aee9d3e82cd49037d8d552e14
邢贵志 <tsing@outlook.com>
2019-05-28 15:48:25 +0800

    1. update color pattern. 2. layout is responsive design now. 3. first step on footer.

---
+++ src/typescript/component/footer.ts
@@ -0,0 +1,11 @@
+export const footer = () => /* html */ `
+  <div class="footer">
+    <div class="footer@total">
+      总计 <span class="amount" data-amount="0"></span>人民币
+    </div>
+    <div class="footer@nav">
+      <a href="" class="prev">上一步</a>
+      <a href="" class="next">下一步</a>
+    </div>
+  </div>
+`;
--- src/typescript/component/main.ts
+++ src/typescript/component/main.ts
@@ -1,5 +1,6 @@
+import { render } from "../helper/utils";
 import { header } from "./header";
 import { model } from "./model";
-import { render } from "../helper/utils";
+import { footer } from "./footer";
 
-document.getElementById("root").innerHTML = render(header(), model());
+document.getElementById("root").innerHTML = render(header(), model(), footer());
