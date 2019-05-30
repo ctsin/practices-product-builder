303382b6281a0e5cb773726fef15f170904409e9
邢贵志 <tsing@outlook.com>
2019-05-30 10:58:39 +0800

    upgrade typescript & config

--- src/typescript/component/main.ts
+++ src/typescript/component/main.ts
@@ -3,4 +3,8 @@ import { header } from "./header";
 import { model } from "./model";
 import { footer } from "./footer";
 
-document.getElementById("root").innerHTML = render(header(), model(), footer());
+document.getElementById("root")!.innerHTML = render(
+  header(),
+  model(),
+  footer()
+);
