74c0eff135091e5536780d5eb1b3ac611c1b4e42
邢贵志 <tsing@outlook.com>
2019-06-04 10:52:57 +0800

    done with selector utils

--- src/typescript/component/main.ts
+++ src/typescript/component/main.ts
@@ -1,10 +1,7 @@
-import { render } from "../helper/utils";
+import { render, $ } from "../helper/utils";
 
 import { header } from "./header";
 import { footer } from "./footer";
 import { body } from "./body";
 
-const root = document.querySelector("#root");
-
-root &&
-  root.insertAdjacentHTML("beforeend", render(header(), body(), footer()));
+$("#root").append(render(header(), body(), footer()));
--- src/typescript/helper/utils.ts
+++ src/typescript/helper/utils.ts
@@ -7,14 +7,14 @@ type On = (
   captureOrOptoins?: boolean | AddEventListenerOptions
 ) => any;
 
-export const on: On = (element, evt, fn, captureOrOptoins): any => {
+export const on: On = (element, evt, fn, captureOrOptoins = false): any => {
   const matches = (eventTarget: EventTarget) =>
     !!(eventTarget as HTMLElement).matches(element);
 
   const delegatorFn = (event: Event) => {
     const { target } = event;
 
-    return matches(target) && fn.call(target, event);
+    return matches(target!) && fn.call(target, event);
   };
 
   document.addEventListener(evt, delegatorFn, captureOrOptoins);
@@ -22,14 +22,41 @@ export const on: On = (element, evt, fn, captureOrOptoins): any => {
   return delegatorFn;
 };
 
-type Off = (
-  eventType: EventType,
-  fn: EventListenerOrEventListenerObject
-) => void;
+type Off = (eventType: EventType, fn: EventListener) => void;
 
 export const off: Off = (eventType, fn) =>
   document.removeEventListener(eventType, fn);
 
 export const isBoolean = (target: any) => typeof target === "boolean";
+
+export const $ = (selector: string, parentNode = document) => {
+  const element = parentNode.querySelectorAll(selector);
+
+  const before = (html: string) => {
+    element.forEach(node => {
+      node.insertAdjacentHTML("beforebegin", html);
+    });
+  };
+
+  const after = (html: string) => {
+    element.forEach(node => {
+      node.insertAdjacentHTML("afterend", html);
+    });
+  };
+
+  const prepend = (html: string) => {
+    element.forEach(node => {
+      node.insertAdjacentHTML("afterbegin", html);
+    });
+  };
+
+  const append = (html: string) => {
+    element.forEach(node => {
+      node.insertAdjacentHTML("beforeend", html);
+    });
+  };
+
+  return { before, after, prepend, append };
+};
 
 export const render = (...templates: string[]) => templates.join("");
